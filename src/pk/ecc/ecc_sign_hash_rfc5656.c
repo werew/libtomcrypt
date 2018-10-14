a24361b4f7a24da5af3f7649a501e408b2e00f65
Karel Miko <karel.miko@gmail.com>
2018-10-14 14:12:30 +0200

    refactor ecc_sign_hash_ex

--- src/pk/ecc/ecc_sign_hash_rfc5656.c
+++ src/pk/ecc/ecc_sign_hash_rfc5656.c
@@ -9,10 +9,10 @@
 
 #include "tomcrypt_private.h"
 
-#ifdef LTC_MECC
+#if defined(LTC_MECC) && defined(LTC_SSH)
 
 /**
-  Sign a message digest (ANSI X9.62 format)
+  Sign a message digest (RFC5656 / SSH format)
   @param in        The message digest to sign
   @param inlen     The length of the digest
   @param out       [out] The destination for the signature
@@ -22,12 +22,14 @@
   @param key       A private ECC key
   @return CRYPT_OK if successful
 */
-int ecc_sign_hash(const unsigned char *in,  unsigned long inlen,
-                  unsigned char *out, unsigned long *outlen,
-                  prng_state *prng, int wprng, const ecc_key *key)
+int ecc_sign_hash_rfc5656(const unsigned char *in,  unsigned long inlen,
+                          unsigned char *out, unsigned long *outlen,
+                          prng_state *prng, int wprng, const ecc_key *key)
 {
    int err;
    void *r, *s;
+   char name[64];
+   unsigned long namelen = sizeof(name);
 
    LTC_ARGCHK(out    != NULL);
    LTC_ARGCHK(outlen != NULL);
@@ -35,11 +37,14 @@ int ecc_sign_hash(const unsigned char *in,  unsigned long inlen,
    if ((err = mp_init_multi(&r, &s, NULL)) != CRYPT_OK) return err;
    if ((err = ecc_sign_hash_internal(in, inlen, r, s, prng, wprng, NULL, key)) != CRYPT_OK) goto error;
 
-   /* store as ASN.1 SEQUENCE { r, s -- integer } */
-   err = der_encode_sequence_multi(out, outlen,
-                                   LTC_ASN1_INTEGER, 1UL, r,
-                                   LTC_ASN1_INTEGER, 1UL, s,
-                                   LTC_ASN1_EOL, 0UL, NULL);
+   /* Get identifier string */
+   if ((err = ecc_ssh_ecdsa_encode_name(name, &namelen, key)) != CRYPT_OK) goto error;
+   /* Store as SSH data sequence, per RFC4251 */
+   err = ssh_encode_sequence_multi(out, outlen,
+                                   LTC_SSHDATA_STRING, name,
+                                   LTC_SSHDATA_MPINT,  r,
+                                   LTC_SSHDATA_MPINT,  s,
+                                   LTC_SSHDATA_EOL,    NULL);
 error:
    mp_clear_multi(r, s, NULL);
    return err;
